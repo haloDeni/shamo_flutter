4fd8d4ab5b96de61db7cbb49215fa45358c7ef06
Deni Nuryadi <Deninuryadi63@gmail.com>
2021-11-04 06:02:25 +0700

    add Carousel

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -2,12 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:shamo_apps/theme.dart';
 
-class ProductPage extends StatelessWidget {
-  const ProductPage({Key? key}) : super(key: key);
-
+class ProductPage extends StatefulWidget {
   @override
+  State<ProductPage> createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  @override
+  List images = [
+    'assets/products/image_51.png',
+    'assets/products/image_38.png',
+    'assets/products/image_31.png',
+  ];
+
+  int currentIndex = 0;
+
   Widget build(BuildContext context) {
+    Widget indicator(int index) {
+      return Container(
+        margin: EdgeInsets.symmetric(horizontal: 2),
+        width: currentIndex == index ? 16 : 4,
+        height: 4,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: currentIndex == index ? primaryColor : Color(0xffC4C4C4),
+        ),
+      );
+    }
+
     Widget header() {
+      int index = -1;
+
       return Container(
         child: Column(
           children: [
@@ -36,7 +61,34 @@ class ProductPage extends StatelessWidget {
                 ],
               ),
             ),
-            CarouselSlider(items: items, options: options)
+            CarouselSlider(
+              items: images
+                  .map((image) => Image.asset(
+                        image,
+                        width: MediaQuery.of(context).size.width,
+                        height: 310,
+                        fit: BoxFit.cover,
+                      ))
+                  .toList(),
+              options: CarouselOptions(
+                initialPage: 0,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: images.map((e) {
+                index++;
+                return indicator(index);
+              }).toList(),
+            ),
           ],
         ),
       );
